7ecb330abccbaf4f78bf565d02055ebb8c3eae55
N0bleKn1ght <tfreite@gmail.com>
2025-01-06 13:59:13 -0500

    Update delete-template.js

--- api/delete-template.js
+++ api/delete-template.js
@@ -4,14 +4,12 @@ const path = require('path');
 module.exports = async (req, res) => {
   if (req.method === 'DELETE') {
     try {
-      const { name } = req.query; // Get the name from the query parameter
+      const { name } = req.query;
       const filePath = path.join(process.cwd(), 'templates.json');
       const templatesData = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
-      // Filter out the template to be deleted
       const updatedTemplates = templatesData.filter(t => t.name !== name);
 
-      // Check if the template was found and deleted
       if (updatedTemplates.length < templatesData.length) {
         fs.writeFileSync(filePath, JSON.stringify(updatedTemplates, null, 2));
         res.status(200).json({ message: 'Template deleted successfully' });
